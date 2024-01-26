238cf1b9c8e42c5f96c3bc86f962efd8f3264aa7
Mohammed <sajidcurious9@gmail.com>
2024-01-27 00:10:37 +0530

    modified

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
+  const amount = useSelector((store) => store.basket.amount);
   return (
     <div className="flex flex-row justify-evenly mt-2 pt-4 text-lg">
       <p>Home</p>
       <p>
         Basket
         <span className="text-xs align-top bg-red-500 text-white rounded-full px-2 py-1 mx-1">
-          0
+          {amount}
         </span>
       </p>
     </div>
