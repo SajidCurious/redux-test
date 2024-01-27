3d652f640b9447b95d053f3c49ceffc7ee68b613
Mohammed <sajidcurious9@gmail.com>
2024-01-27 14:13:27 +0530

    added data

---
+++ src/redux/data.js
@@ -0,0 +1,32 @@
+export const storeData = [
+  {
+    name: Flower,
+    price: 100,
+    image:
+      "https://img.freepik.com/free-photo/purple-osteospermum-daisy-flower_1373-16.jpg",
+  },
+  {
+    name: Phone,
+    price: 150,
+    image:
+      "https://img.freepik.com/free-photo/purple-osteospermum-daisy-flower_1373-16.jpg",
+  },
+  {
+    name: Bike,
+    price: 200,
+    image:
+      "https://img.freepik.com/free-photo/purple-osteospermum-daisy-flower_1373-16.jpg",
+  },
+  {
+    name: Socket,
+    price: 250,
+    image:
+      "https://img.freepik.com/free-photo/purple-osteospermum-daisy-flower_1373-16.jpg",
+  },
+  {
+    name: Charger,
+    price: 300,
+    image:
+      "https://img.freepik.com/free-photo/purple-osteospermum-daisy-flower_1373-16.jpg",
+  },
+];
