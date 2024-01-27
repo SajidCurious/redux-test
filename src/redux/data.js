9f549957ceb1965c7eddd2857126cfd2bcf12df1
Mohammed <sajidcurious9@gmail.com>
2024-01-27 14:20:24 +0530

    modified

--- src/redux/data.js
+++ src/redux/data.js
@@ -4,29 +4,33 @@ export const storeData = [
     price: 100,
     image:
       "https://img.freepik.com/free-photo/purple-osteospermum-daisy-flower_1373-16.jpg",
+    amount: 1,
   },
   {
     name: Phone,
     price: 150,
-    image:
-      "https://img.freepik.com/free-photo/purple-osteospermum-daisy-flower_1373-16.jpg",
+    image: "https://www.pcmag.com/picks/the-best-cheap-phones",
+    amount: 1,
   },
   {
     name: Bike,
     price: 200,
     image:
-      "https://img.freepik.com/free-photo/purple-osteospermum-daisy-flower_1373-16.jpg",
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.carandbike.com%2Fnew-bikes%2Fmodels&psig=AOvVaw2QM561Hzhdoc-S4NQ_PYwm&ust=1706431541535000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCICVt4OX_YMDFQAAAAAdAAAAABAJ",
+    amount: 1,
   },
   {
     name: Socket,
     price: 250,
     image:
-      "https://img.freepik.com/free-photo/purple-osteospermum-daisy-flower_1373-16.jpg",
+      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.jiomart.com%2Fimages%2Fproduct%2Foriginal%2Frvuunu1bqo%2Fmx-3-pin-type-d-universal-socket-connector-converts-5-amps-to-15-amps-pc-moulded-mx-2766-product-images-orvuunu1bqo-p594411800-0-202210111843.jpg%3Fim%3DResize%3D(420%2C420)&tbnid=FZfVbeDwDaAsqM&vet=12ahUKEwjxqa6el_2DAxVFWmwGHddvD4wQMygNegUIARCUAQ..i&imgrefurl=https%3A%2F%2Fwww.jiomart.com%2Fp%2Fgroceries%2Fmx-3-pin-type-d-universal-socket-connector-converts-5-amps-to-15-amps-pc-moulded-mx-2766%2F594411800&docid=ucosX_7VqY7DzM&w=420&h=420&q=Socket%20images&ved=2ahUKEwjxqa6el_2DAxVFWmwGHddvD4wQMygNegUIARCUAQ",
+    amount: 1,
   },
   {
     name: Charger,
     price: 300,
     image:
-      "https://img.freepik.com/free-photo/purple-osteospermum-daisy-flower_1373-16.jpg",
+      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fm.media-amazon.com%2Fimages%2FI%2F61NfFCwsneL._AC_UF1000%2C1000_QL80_.jpg&tbnid=-Td4Uq8wAFQvAM&vet=12ahUKEwj5mtGrl_2DAxX0amwGHQ76AIQQMygBegQIARB2..i&imgrefurl=https%3A%2F%2Fwww.amazon.in%2FCharger-Adapter-Mobile-Android-Charging%2Fdp%2FB0813WBJ6B&docid=73E0BGLCuHIAxM&w=1000&h=930&q=charger%20images&ved=2ahUKEwj5mtGrl_2DAxX0amwGHQ76AIQQMygBegQIARB2",
+    amount: 1,
   },
 ];
