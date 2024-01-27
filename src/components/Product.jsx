45de018763d3ae23889e3a92a6ad6810ac284cb2
Mohammed <sajidcurious9@gmail.com>
2024-01-27 16:30:45 +0530

    buildng product component

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-const Product = () => {
-  return <div>Product</div>;
+const Product = ({ name, price, image, amount }) => {
+  return (
+    <div className="flex flex-row items-center gap-8 px-10 mt-20">
+      <img src={image} alt={name} className="w-40 h-40" />
+      <div className="w-1/2 px-10">
+        <p className="text-xl font-medium">{name}</p>
+        <p className="text-lg">{price.toFixed(2)}</p>
+      </div>
+      <div className="flex flex-col items-center">
+        <p className="text-lg font-medium">Amount</p>
+        <p>{amount}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
