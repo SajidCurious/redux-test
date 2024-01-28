7783767ae2e4dd5d9bd1a3aaa530c89ab0da095b
Mohammed <sajidcurious9@gmail.com>
2024-01-28 16:23:59 +0530

    used useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { useEffect } from "react";
 import BasketProducts from "./components/BasketProducts";
 import Navbar from "./components/Navbar";
+import { updateTotal } from "./features/basketSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const { products } = useSelector((store) => store.basket);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(updateTotal());
+  }, [products, dispatch]);
   return (
     <>
       <Navbar />
